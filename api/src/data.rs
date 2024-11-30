d5f6f6ab522c548cf693cd78c67722815d4c02ee
Aidan Wallace <77929240+Aidan-Wallace@users.noreply.github.com>
2024-11-30 00:15:32 -0500

    api and ui

---
+++ api/src/data.rs
@@ -0,0 +1,14 @@
+use serde::Deserialize;
+
+pub struct AppState {
+    pub html: String,
+}
+
+#[derive(Deserialize)]
+pub struct GenerateRandom {
+    pub width: u16,
+    pub height: u16,
+}
+
+#[derive(Deserialize)]
+pub struct InputData(pub Vec<Vec<u8>>);
---
+++ api/src/main.rs
@@ -0,0 +1,66 @@
+mod data;
+
+use actix_cors::Cors;
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use data::{AppState, GenerateRandom, InputData};
+use game_of_life::matrix;
+use std::{env, fs};
+
+const HOST: &str = "127.0.0.1";
+const DEFAULT_PORT: u16 = 8080;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let host = env::var("HOST").unwrap_or(HOST.to_string());
+    let port = match env::var("PORT").unwrap_or(DEFAULT_PORT.to_string()).parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!(
+                "Failed to parse PORT. Falling back to default: {}",
+                DEFAULT_PORT
+            );
+            DEFAULT_PORT
+        }
+    };
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Cors::permissive())
+            .app_data(web::Data::new(AppState {
+                html: match fs::read_to_string("index.html") {
+                    Ok(content) => content,
+                    Err(_) => {
+                        panic!("Could not read html from index.html")
+                    }
+                },
+            }))
+            .route("/", web::get().to(index))
+            .route("/generate-random", web::get().to(generate_random))
+            .service(update_board)
+    })
+    .bind((host, port))?
+    .run()
+    .await
+}
+
+#[post("/check")]
+async fn update_board(input: web::Json<InputData>) -> impl Responder {
+    let mut matrix = matrix::Matrix::new(input.into_inner().0);
+
+    game_of_life::convert(&mut matrix);
+
+    web::Json(matrix.data)
+}
+
+async fn generate_random(query: web::Query<GenerateRandom>) -> impl Responder {
+    let result = matrix::Matrix::generate_random_binary(query.width, query.height);
+
+    web::Json(result.data)
+}
+
+async fn index(data: web::Data<AppState>) -> impl Responder {
+    // Return the HTML content as a response
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(data.html.clone())
+}
